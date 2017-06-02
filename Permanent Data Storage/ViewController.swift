74e21bcdeddc45ba01413b5c437056466c52b19d
RASHED <hello@MacBooks-MacBook-Pro.local>
2017-06-02 16:27:28 +0600

    permanent data storage first commit

--- Permanent Data Storage/ViewController.swift
+++ Permanent Data Storage/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var numberField: UITextField!
+    
+    @IBAction func save(_ sender: Any) {
+        
+//    UserDefaults.standard.set(numberField.text, forKey: "number")
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let nameObject = UserDefaults.standard.object(forKey: "name")
+//        
+//        if let name = nameObject as? String {
+//            print(name)
+//        }
+//        
+//        let arr = [1,2,3,4]
+//        
+//        //UserDefaults.standard.set(arr, forKey: "array")
+//        
+//        let arrayObject = UserDefaults.standard.object(forKey: "array")
+//        
+//        if let array = arrayObject as? NSArray {
+//          print(array)
+//        }
+        
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+        
+        if let number = numberObject as? String {
+        
+            numberField.text = number
+        }
+        
+        
+        
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
